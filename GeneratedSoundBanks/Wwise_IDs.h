38547e58aaafeb5857d71d79df92c99ddc16977c
DESKTOP-URLPBNP\marie <marievalentinedavid@gmail.com>
2018-05-25 14:31:41 +0200

    S1 et S1bis update

---
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID H_DETECTEDFRAGMENT = 3534651088U;
+        static const AkUniqueID H_EXPLOREROOM = 1450671466U;
+        static const AkUniqueID H_OPTIMIZEMOVE = 783952308U;
+        static const AkUniqueID H_TARGETANDCLICK = 2138475376U;
+        static const AkUniqueID H_TWOLINKCROSSED = 1195339271U;
+        static const AkUniqueID L1_S1_FAILEUBE = 1109362563U;
+        static const AkUniqueID L1_S1BIS_HILTRUDE = 3672070969U;
+        static const AkUniqueID STOP_UI_LINKHOLD = 8245926U;
+        static const AkUniqueID T_ACTIVLINK = 2016698951U;
+        static const AkUniqueID T_COMMONASPECT = 1158054437U;
+        static const AkUniqueID T_ENERGYGAUGE = 1438124155U;
+        static const AkUniqueID T_FIRSTFRAGMENT = 1530286288U;
+        static const AkUniqueID T_FRAGMENTLINKED = 525151809U;
+        static const AkUniqueID T_FRAGMENTLINKEDCONTINUE = 296874062U;
+        static const AkUniqueID T_LINKTWOFRAGMENT = 1326074304U;
+        static const AkUniqueID T_MEMORIALFLOW = 3038282616U;
+        static const AkUniqueID T_SCANTOMOVE = 781812845U;
+        static const AkUniqueID T_SECONDFRAGMENT = 3389379746U;
+        static const AkUniqueID T_TEMPORALSEGMENT = 145059539U;
+        static const AkUniqueID T_WELCOME = 1834850122U;
+        static const AkUniqueID UI_CANCELEDLINK = 1170520947U;
+        static const AkUniqueID UI_CLIC = 4076284683U;
+        static const AkUniqueID UI_COMPLETEDMEMORY = 3570630340U;
+        static const AkUniqueID UI_CROSSEDLINK = 107704913U;
+        static const AkUniqueID UI_ENERGYGAUGEINCREASE = 1192864647U;
+        static const AkUniqueID UI_LAUNCHEDGAME = 3276648742U;
+        static const AkUniqueID UI_LINKHOLD = 2567115677U;
+        static const AkUniqueID UI_MENURETURN = 823617801U;
+        static const AkUniqueID UI_MENUVALIDATE = 20648997U;
+        static const AkUniqueID UI_NEWMEMORYAPPEAR = 4278458338U;
+        static const AkUniqueID UI_NOTENOUGHENERGY = 811615621U;
+        static const AkUniqueID UI_ROOMSCANNING = 2697614442U;
+        static const AkUniqueID UI_VALIDATEDLINK = 1853066436U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID S1 = 1584861493U;
+        static const AkUniqueID S2 = 1584861494U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
